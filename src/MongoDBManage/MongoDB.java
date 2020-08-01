fa2edabc784ef742740095118f209574e4288975
bvd5283 <bvd5283@psu.edu>
2020-07-31 22:53:20 -0400

    Call-for-Code

--- src/MongoDBManage/MongoDB.java
+++ src/MongoDBManage/MongoDB.java
@@ -31,8 +31,8 @@ public class MongoDB {
      
         //MongoClient mongoClient = MongoClients.create("mongodb+srv://xxxxxx:xxxxxx@cluster0.lwtzc.mongodb.net/test?retryWrites=true&w=majority");
     
-       MongoClient mongoClient = MongoClients.create("mongodb://ibm_cloud_58952a88_e85c_4296_a246_749125551280:7165f83e5cf6a3a724de9a2306224206f493e30936c3467b7ee09f77b41e3be0@49101ee2-9b6e-44f7-8839-48152408532d-0.brjdfmfw09op3teml03g.databases.appdomain.cloud:30679,49101ee2-9b6e-44f7-8839-48152408532d-1.brjdfmfw09op3teml03g.databases.appdomain.cloud:30679/AQUADB?authSource=admin&replicaSet=replset&tlsInsecure=true&ssl=true");
-       //If you need to connect to the database, please contact us. Also you need to have an account for IBM cloud.
+       MongoClient mongoClient = MongoClients.create("");
+       //For security reasons, if you need to connect to the database, please contact us. Also you need to have an account for IBM cloud.
 
        mongoDatabase = mongoClient.getDatabase(databaseName);  
        //System.out.println("Connect to database successfully");
