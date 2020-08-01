550a94aaab050b9c3c68bcacf3096584fd34072d
bvd5283 <bvd5283@psu.edu>
2020-07-31 22:36:14 -0400

    Call-for-Code

--- src/MongoDBManage/MongoDB.java
+++ src/MongoDBManage/MongoDB.java
@@ -27,17 +27,13 @@ public class MongoDB {
     public static MongoDatabase getConnection()
     {
          
-        // connect to mongodb service
-    //    MongoClient mongoClient = MongoClients.create();
+    
      
-        MongoClient mongoClient = MongoClients.create("mongodb+srv://cdhdnsdbn:Jane621027@cluster0.lwtzc.mongodb.net/test?retryWrites=true&w=majority");
-       // ConnectionString connString = new ConnectionString(
-       //"mongodb://ibm_cloud_58952a88_e85c_4296_a246_749125551280:7165f83e5cf6a3a724de9a2306224206f493e30936c3467b7ee09f77b41e3be0@49101ee2-9b6e-44f7-8839-48152408532d-0.brjdfmfw09op3teml03g.databases.appdomain.cloud:30679,49101ee2-9b6e-44f7-8839-48152408532d-1.brjdfmfw09op3teml03g.databases.appdomain.cloud:30679/AQUADB?authSource=admin&replicaSet=replset&tlsInsecure=true&ssl=true"
-       //);
+        //MongoClient mongoClient = MongoClients.create("mongodb+srv://xxxxxx:xxxxxx@cluster0.lwtzc.mongodb.net/test?retryWrites=true&w=majority");
+    
+       MongoClient mongoClient = MongoClients.create("mongodb://ibm_cloud_58952a88_e85c_4296_a246_749125551280:7165f83e5cf6a3a724de9a2306224206f493e30936c3467b7ee09f77b41e3be0@49101ee2-9b6e-44f7-8839-48152408532d-0.brjdfmfw09op3teml03g.databases.appdomain.cloud:30679,49101ee2-9b6e-44f7-8839-48152408532d-1.brjdfmfw09op3teml03g.databases.appdomain.cloud:30679/AQUADB?authSource=admin&replicaSet=replset&tlsInsecure=true&ssl=true");
+       //If you need to connect to the database, please contact us. Also you need to have an account for IBM cloud.
 
-       //MongoClient mongoClient = MongoClients.create(connString);
-
-       //MongoClient mongoClient = MongoClients.create("mongodb://ibm_cloud_58952a88_e85c_4296_a246_749125551280:7165f83e5cf6a3a724de9a2306224206f493e30936c3467b7ee09f77b41e3be0@49101ee2-9b6e-44f7-8839-48152408532d-0.brjdfmfw09op3teml03g.databases.appdomain.cloud:30679,49101ee2-9b6e-44f7-8839-48152408532d-1.brjdfmfw09op3teml03g.databases.appdomain.cloud:30679/ibmclouddb?authSource=admin&authMechanism=SCRAM-SHA-256&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=true");
        mongoDatabase = mongoClient.getDatabase(databaseName);  
        //System.out.println("Connect to database successfully");
        return mongoDatabase;
